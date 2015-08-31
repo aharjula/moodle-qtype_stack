99472be4e81dc8cb420bd456df911fe56495cfd1
Matti Harjula <matti.harjula@aalto.fi>
2015-08-31 16:37:53 +0300

    Fixed some missing imports in the tests.
    Tried parallel-phpunit but as it was running tests one file at a time imports defined in other files were not present... not an issue as we wont actually gain anything from this when there is a database in use and the test block eachother, maybe multiple virtual machines and multiple databases and just split the tests among them.

--- tests/stack_options_test.php
+++ tests/stack_options_test.php
@@ -21,6 +21,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+require_once(__DIR__ . '/../locallib.php');
 require_once(__DIR__ . '/../stack/options.class.php');
 
 
